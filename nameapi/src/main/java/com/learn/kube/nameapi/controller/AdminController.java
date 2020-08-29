5e75e7e6fc7c0af81c140bf29a00cfa839ea370e
Mamoon Habib <mamoon.habib@gmail.com>
2020-08-29 15:53:24 -0700

    Add version info

---
+++ nameapi/src/main/java/com/learn/kube/nameapi/controller/AdminController.java
@@ -0,0 +1,21 @@
+package com.learn.kube.nameapi.controller;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+class AdminController {
+
+  @GetMapping("/version/info")
+  Map<String, String> geVersiont() {
+
+    HashMap<String, String> map = new HashMap<>();
+    map.put("version", "2.1-"+new Date().getTime());
+    return map;
+  }
+}
